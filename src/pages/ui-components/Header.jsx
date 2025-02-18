764c1166d8e4814975a5ca2d67a582e02d9f53a6
Andres Escala <aescalaa1900@alumno.ipn.mx>
2025-02-17 23:02:38 -0600

    refactor: modify placeholder, add new font

--- src/pages/ui-components/Header.jsx
+++ src/pages/ui-components/Header.jsx
@@ -7,10 +7,9 @@ const Header = () => {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
                         {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eliptica</span>
+                        <span id="eliptica-logo" className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Eliptica</span>
                     </a>
                     <div className="flex md:order-2">
-                        <form className=""></form>
                         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -24,11 +23,12 @@ const Header = () => {
                                 </svg>
                                 <span className="sr-only">Search icon</span>
                             </div>
-                            <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                            <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Busca tus prendas..." />
                         </div>
                         <div className="flex items-center space-x-6 rtl:space-x-reverse">
-                            <a href="tel:5541251234" className="text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-                            <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+                            <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">
+                                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inicia sesión</button>
+                            </a>
                         </div>
                     </div>
                 </div>
