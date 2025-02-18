7c371a6504936d08f010e9ffecab09445ec29362
Andres Escala <aescalaa1900@alumno.ipn.mx>
2025-02-17 23:05:31 -0600

    modify: new icons added

--- src/pages/ui-components/Header.jsx
+++ src/pages/ui-components/Header.jsx
@@ -29,6 +29,8 @@ const Header = () => {
                             <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">
                                     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Inicia sesión</button>
                             </a>
+                            <i className="fa-regular fa-heart"></i>
+                            <i className="fa-solid fa-bag-shopping"></i>
                         </div>
                     </div>
                 </div>
